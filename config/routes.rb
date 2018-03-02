c0da3beb6c787967086159017c240d8d14548183
David Morley <davidkmorley@gmail.com>
2018-03-02 14:13:08 +0000

    added bookings viewer per user- linked in dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :items do
     resources :bookings, only: [:new, :create, :index, :show]
+    get 'index_by_user', to: "bookings#index_by_user"
   end
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
